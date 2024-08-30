e28ba13377dd5b896a1bdf434ba9c51c2810d74b
saidkabirorakzai <saidkabirorakzai7@gmail.com>
2024-08-30 06:28:54 +0100

    add all

---
+++ sk.js
@@ -0,0 +1,47 @@
+/*let fruits=["mango","apple","grapes","orange","banana"]
+fruits.shift()
+fruits.unshift("mango")
+console.log(fruits)
+
+for ( let index=0; index <=100; index++){
+    console.log(index)
+}
+
+for ( let index=0; index <=10; index++){
+    console.log(index + " x " +" 2 "+" = " + index * 2)
+}
+for ( let index=10; index >=0; index--){
+    console.log(index + " x " +" 2 "+" = " + index * 2)
+}
+let array=["one","two","three","four"]
+
+for ( let index=0; index <array.length; index++){
+    console.log(array[0],array[1],array[2],array[3],array[4])
+}
+
+let array=["one","two","three","four"]
+
+for ( let index=0; index <array.length; index++){
+    console.log(array[index])
+}
+
+let firstname=["said","ali","umar","amir"]
+let lastname=["kabir","khan","amin","usama"]
+let fullname=[]; 
+for(let i=0; i < firstname.length; i++){
+    for(let = 0 ;j<  lastname.length; j++){
+
+if(i==j){
+
+        fullname.push(firstname[i] + lastname[j]);
+      }
+    }
+
+}*/
+
+let firstname = "said"
+console.log(firstname.toLowerCase())
+
+
+
+
